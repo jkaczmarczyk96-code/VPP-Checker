cb9a4b85983d7559286c9376948b052abb211dff
jkaczmarczyk96-code <j.kaczmarczyk96@gmail.com>
2026-04-21 00:12:06 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://vpp-checker.vercel.app",
+        "https://www.vpp-checker.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
